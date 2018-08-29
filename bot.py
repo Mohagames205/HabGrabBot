c0c61f17ff24cc13759f5f73d7bf2522de462ccd
Mohagames205 <40402787+Mohagames205@users.noreply.github.com>
2018-08-29 18:04:09 +0200

    Help command aangepast

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ async def grab(ctx, habbo):
 @bot.command(pass_context = True)
 async def help(ctx):
 	await bot.say("Deze command is nog in onderhoud")
-	embed=discord.Embed(title="Help", description="Hier zie je alle botcommands", color=0xffff00)
+	embed=discord.Embed(title="Er zijn intotaal 2 commands", color=0xffff00)
 	embed.add_field(name="$grab (habbonaam)", value="Geeft informatie over de gebruiker die je opzoekt.")
 	embed.add_field(name="$help", value="Geeft je alle verschillende commands")
 	embed.add_field(name="Vragen over de bot?", value="Contacteer @Mohagames#7389 voor hulp en antwoorden op je vragen.")
