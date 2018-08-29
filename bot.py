bd070768cbd8ca1854fa63983cbf3d65038ceb37
Mohagames205 <40402787+Mohagames205@users.noreply.github.com>
2018-08-29 15:18:56 +0200

    test

--- bot.py
+++ bot.py
@@ -48,6 +48,10 @@ async def grab(ctx, habbo):
         else:
             status = ("De gebruiker is onzichtbaar")
         await bot.say(f"**Gegevens van de Habbo:**\n **Habbonaam:** {naamio}\n **Motto:** {motto}\n **Lid sinds:** {lidsince}\n **Zichtbaarheid:** {status}")
+		embed = discord.Embed(title="Hier zijn alle gegevens van de opgevraagde Habbo:", colour=discord.Colour(0xe9d166), timestamp=datetime.datetime.utcfromtimestamp(1535548044))
+		embed.set_thumbnail(url=f"https://www.habbo.nl/habbo-imaging/avatarimage?hb=image&user={habbo}")
+
+		await bot.say(embed=embed)
 	
 	
 
