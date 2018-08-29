804ee8a652683285309673e05a4a4f708abde946
Mohagames205 <40402787+Mohagames205@users.noreply.github.com>
2018-08-29 13:39:19 +0200

    Added bot.py file

---
+++ bot.py
@@ -0,0 +1,56 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import random
+import json
+import os
+import datetime
+import traceback
+import urllib.request, json
+#tijd = int(time.strftime("%M"))
+#tijd1 = str(time.strftime("%c"))
+#tijd1 = datetime.datetime.now().strftime("%c")
+cp = '$'
+bot = commands.Bot(command_prefix=cp)
+
+print ('Bot is aan het laden...')
+print ('.....')
+print ('Het laden is bijna compleet!')
+print ('Bij errors exit invoeren in console!')
+print ('De command prefix die wordt gebruikt is ' + cp)
+print ('...')
+print ('...')
+print ('We zijn er bijna!')
+print ('Ter info! De bot is niet bedoeld als U-bot maar als Discord Bot')
+print ('Versie 1.3')
+print ('Nieuwe logo en naam')
+@bot.event
+async def on_ready():
+    await bot.edit_profile(username="HabGrab")
+    #await bot.change_presence(game=discord.Game(name='Use {}help | Aardappelen'.format(cp)))
+    print('Bot is geladen als')
+    print(bot.user.name)
+    print(bot.user.id)
+	
+    
+	
+@bot.command(pass_context = True)
+async def grab(ctx, habbo):
+    with urllib.request.urlopen(f"https://www.habbo.nl/api/public/users?name={habbo}") as url:
+        data = json.loads(url.read().decode())
+        naamio = data['name']
+        motto = data['motto']
+        lidsince = data['memberSince']
+        status = data['profileVisible']
+        if status == True:
+            status = ("De gebruiker is zichtbaar")
+        else:
+            status = ("De gebruiker is onzichtbaar")
+        await bot.say(f"  **Habbonaam:** {naamio}\n **Motto:** {motto}\n **Lid sinds:** {lidsince}\n **Zichtbaarheid:** {status}")
+	
+
+
+	
+
+bot.run("")
