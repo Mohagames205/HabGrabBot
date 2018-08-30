809fb300a6e6bf2e1e6da905e22ec34af5040d1d
Mohagames205 <40402787+Mohagames205@users.noreply.github.com>
2018-08-30 14:13:04 +0200

    Added avatar support

--- bot.py
+++ bot.py
@@ -47,13 +47,15 @@ async def grab(ctx, habbo):
 			embed.add_field(name="Lid sinds:" , value=lidsince, inline=True)
 			embed.add_field(name="Zichtbaarheid:" , value=status, inline=True)
 			embed.set_footer(text="Powered by HabGrab ©")
+			channel = ctx.message.channel
 			await bot.say(embed=embed)
+			await bot.send_file(channel, "gabbo.jpg", content="Avatar:", filename="gabbo.jpg")
 	except:
 		embed=discord.Embed(title="Er is iets misgelopen", description="De gebruiker bestaat niet", color=0xffff00)
 		embed.set_thumbnail(url="http://justcakenl.tk/host/events.gif")
 		embed.set_footer(text="Powered by HabGrab ©")
 		await bot.say(embed=embed)
-	
+		
 @bot.command(pass_context = True)
 async def info(ctx):
 	embed=discord.Embed(title="HabGrab info", description="Alle informatie die je zou willen weten over HabGrab", color=0xffff00)
